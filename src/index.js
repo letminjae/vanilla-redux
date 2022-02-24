25778e07bbbb8196ac8c7cdae11ba46ef0cedaa7
letminjae <cmjj0824@naver.com>
2022-02-25 02:39:00 +0900

    add, delete redux

--- src/index.js
+++ src/index.js
@@ -1,46 +1,90 @@
 import { createStore } from "redux";
-
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
 //액션
-const ADD = "ADD"
-const MINUS = "MINUS"
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+//액션생성함수
+const addToDoAction = (text) => {
+  return {
+    type: ADD_TODO,
+    text 
+  }
+}
+
+const deleteToDoAction = (id) => {
+  return {
+    type: DELETE_TODO,
+    id
+  }
+}
 
 
-//리듀서 switch 타입
-const reducer = (count = 0, action) => {
-  switch (action.type){
-    case ADD :
-      return count + 1;
-    case MINUS :
-      return count - 1;
+//리듀서
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, {text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      //delete는 filter를 쓰면 무조건 새로운 배열로 나온다 그래서 filter를 쓴다
+      //filter에 첫인자는 모든 투두들을 담고, 우리가 유지시켜야할 todo를 새배열로 받아와야함
+      return state.filter(toDo => toDo.id !== parseInt(action.id))
     default:
-      return count;
+      return state;
   }
 };
 
-//스토어
-const countStore = createStore(reducer);
+const store = createStore(reducer);
 
-//store.subscribe = store의 변화를 감지해주는 메서드
-//onchange가 발동될때마다 subscribe가 store의 변화를 감지
-const onChange = () => {
-  number.innerText = countStore.getState()
-}
-countStore.subscribe(onChange);
+store.subscribe(() => console.log(store.getState()));
 
-//이벤트 핸들러 => 내가 자주보는 컴포넌트에서의 이벤트 값들
-//여기선 액션타입값을 주었지만 컴포넌트에선 액션생성함수를 받아옴
-const handleAdd = () => {
-  countStore.dispatch({type: ADD })
+//여기서 text는 write to do에 뭐가 쓰일지를 넣는 인자다
+// 왜냐면 리듀서에 text와 id값을 넣어서 state를 변화시킨다 했으니
+// dispatch 보낼때 앞단에서 변화되는 text값을 같이 넣어서 보내줘야되기 때문
+const addTodo = (text) => {
+  store.dispatch(addToDoAction(text));
 }
 
-const handleMinus = () => {
-  countStore.dispatch({type: MINUS })
+//그럼 삭제는? 고유한 id값을 삭제하면 되지않겟어?
+const deleteToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteToDoAction(id))
 }
 
-add.addEventListener("click", handleAdd)
+//앞단에다가 표시해주기
+const paintToDos = () => {
+  //지금 변화한 state값
+  const toDos = store.getState();
+  //ul값 리셋시키기
+  ul.innerHTML = "";
+  //반복문 돌리기
+  toDos.forEach(toDo => {
+    //변수 li는 옆에 점 생성하는거임, btn은 삭제버튼
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", deleteToDo);
+    li.id = toDo.id
+    li.innerText = toDo.text;
+    //ul에다가 li 찍어주기 li옆에는 버튼만들기
+    li.appendChild(btn);
+    ul.appendChild(li);
+  })
+}
 
-minus.addEventListener("click", handleMinus)
+store.subscribe(paintToDos);
+
+
+
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  addTodo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);
